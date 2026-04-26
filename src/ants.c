00688155fd58af89633e4d19a340fc8b9d0fd936
OnlySash <schavar03@gmail.com>
2026-04-25 23:24:29 -0600

    base del proyecto bajo estructura

---
+++ src/ants.c
@@ -0,0 +1,24 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+void ant_creation(){
+      //t_nu, cantidad de hilos
+  //pthread_t t_ids[t_num];
+  //pthread_mutex_init(&mutex, NULL); 
+
+  // Crear cada hilo en simultaneo
+  //for (int t = 0; t < t_num; ++t) {
+    //data *t_data = malloc(sizeof(data));
+
+    //t_data->array = arr;
+
+    //pthread_create(&t_ids[t], NULL, ant_creation, NULL);
+  //}
+
+  //for (int t = 0; t < t_num; ++t) {
+    //pthread_join(t_ids[t], NULL);
+  //}
+}
---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+pthread_mutex_t *mutex;
+
+int main() {
+
+
+  return 0;
+}

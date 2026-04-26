53fea006d614a79be8befa0eb5351f462ba3b583
OnlySash <schavar03@gmail.com>
2026-04-26 00:41:17 -0600

    integraciòn en el proyecto base

--- src/main.c
+++ src/main.c
@@ -1,12 +1,12 @@
-#include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-pthread_mutex_t *mutex;
+#include "environment.h"
 
 int main() {
+  environment_t env;
+  int size = 35;
 
+  environment_init(&env, size, ENVIRONMENT_MAZE);
+  environment_show(&env);
+  environment_dest(&env);
 
   return 0;
 }

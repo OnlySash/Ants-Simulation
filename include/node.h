3bff860c59190d81dbc5318faceef24924c7b6ff
Arturo Alfaro <arturo.alfarodiaz@ucr.ac.cr>
2026-04-27 22:28:28 -0600

    Headers de arbol y nodos

---
+++ include/node.h
@@ -0,0 +1,19 @@
+#ifndef NODE_H
+#define NODE_H
+
+#include <pthread.h>
+
+typedef struct node {
+    int id;
+    int food;
+    int child_count;
+    int child_capacity;
+    struct node **children;
+} node_t;
+
+node_t *node_create(int id, int child_capacity);
+int node_add_child(node_t *parent, node_t *child);
+int node_is_leaf(const node_t *node);
+void node_destroy(node_t *node);
+
+#endif
---
+++ include/tree.h
@@ -0,0 +1,20 @@
+#ifndef TREE_H
+#define TREE_H
+
+#include "node.h"
+
+typedef struct tree {
+    node_t *root;
+    node_t **nodes;
+    int node_count;
+    int initial_food;
+    int max_children;
+} tree_t;
+
+tree_t *tree_create_random(int node_count, int max_children);
+void tree_assign_food_to_leaves(tree_t *tree);
+node_t *tree_walk_random_leaf(tree_t *tree, unsigned int *seed);
+void tree_destroy(tree_t *tree);
+void tree_print_summary(const tree_t *tree);
+
+#endif
